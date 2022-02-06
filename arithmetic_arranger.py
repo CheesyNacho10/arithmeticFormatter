e5e1928e5582bba2b6445102fd932d7a47b26cd3
Nacho Diez <67198608+CheesyNacho10@users.noreply.github.com>
2022-02-06 14:08:19 +0000

    Added the proper tests and also fixed the bugs that couldn't be dettected before

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,36 +1,32 @@
 def arithmetic_arranger(problems, resolve=False):
   if (len(problems) > 5): 
-    print("Error: Too many problems.")
-    return
+    return "Error: Too many problems."
+    
   output = "\n\n\n" if resolve else "\n\n"
   for problem in problems:
     output = output.split('\n')
     problem = problem.split()
 
     if (not problem[0].isdigit() or not problem[2].isdigit()):
-      print("Error: Numbers must only contain digits.")
-      return
+      return "Error: Numbers must only contain digits."
     if (not problem[1] == '+' and not problem[1] == '-'):
-      print("Error: Operator must be '+' or '-'.")
-      return
-    
-    longest_number = len(max(problem))
+      return "Error: Operator must be '+' or '-'."
+
+    longest_number = len(max(problem, key=len))
     if (longest_number > 4) :
-      print("Error: Numbers cannot be more than four digits.")
-      return
+      return "Error: Numbers cannot be more than four digits."
     
     for s in range(longest_number +  2 - len(problem[0])):
       output[0] += " "
-    output[0] += problem[0] + "\t"
+    output[0] += problem[0]
 
     output[1] += problem[1]
     for s in range(longest_number +  1 - len(problem[2])):
       output[1] += " "
-    output[1] += problem[2] + "\t"
+    output[1] += problem[2]
 
     for s in range(longest_number +  2):
       output[2] += "-"
-    output[2] += "\t"
     
     if (resolve):
       result = int()
@@ -40,6 +36,14 @@ def arithmetic_arranger(problems, resolve=False):
         result += (int(problem[0]) - int(problem[2]))
       for s in range(longest_number +  2 - len(str(result))):
         output[3] += " "
-      output[3] += str(result) + "\t"
-    output = '\n'.join(output)
-  print(output)
+      output[3] += str(result) + "    "
+    else :
+      output[2] += "    "
+    
+    output = '    \n'.join(output)
+
+  output = output.split('\n')
+  for l in range(len(output)):
+    output[l] = output[l].rstrip()
+  output = '\n'.join(output)
+  return output
--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-from arithmetic_arranger import *
+# This entrypoint file to be used in development. Start by reading README.md
+from pytest import main
 
-# Correct test
-arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
-print("\n")
-arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
-print("\n")
-arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True)
-print("\n")
-arithmetic_arranger(["12 + 33", "2453 - 23", "1 + 235", "200 - 1"], True)
-print("\n")
+from arithmetic_arranger import arithmetic_arranger
 
-# Fail tests
-arithmetic_arranger(["12 + 33", "2453 - 23", "1 + 235", "200 - 1","32 + 698", "3801 - 2", "45 + 43"], True)
-arithmetic_arranger(["121235 + 12323133", "25344453 - 123323", "1234342 + 265435", "22342300 - 1566"], True)
-arithmetic_arranger(["12 / 33", "2453 * 23", "1 / 235", "200 * 1"], True)
-arithmetic_arranger(["aD + c#", "hgf - sd", "two + three", "four - one"], True)
+
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+
+
+# Run unit tests automatically
+main()
